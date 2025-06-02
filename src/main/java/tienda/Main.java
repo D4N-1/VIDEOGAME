3b87c33f923c97529a5f82d1270ed5f9c1663359
D4N1 <dannyandrestecnologia@outlook.com>
2025-06-01 19:08:25 -0500

    Actualizacion del main con los controller y service

--- src/main/java/tienda/Main.java
+++ src/main/java/tienda/Main.java
@@ -2,12 +2,33 @@ package tienda;
 
 import io.javalin.Javalin;
 import tienda.controlador.UsuarioController;  // <--- Aquí va el punto y coma
+import tienda.controlador.exceptionController;
+//import tienda.controlador.HealthController;
+//import tienda.controlador.ProductoController;
+import tienda.repositorio.ProductosRepository;
+//import tienda.service.ProductoService;
+import tienda.servicio.ProductoService;
 
 public class Main {
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(7000);
+        ProductosRepository productosRepository = new ProductosRepository();
+        ProductoService productoService = new ProductoService(productosRepository);
+        /*ProductoController productoController = new ProductoController(productoService);
+        ExceptionController exceptionController = new ExceptionController();
+        HealthController healthController = new HealthController();
+        */
 
-        UsuarioController usuarioController = new UsuarioController();
-        usuarioController.registerRoutes(app);
+        var app = Javalin.create();
+
+        /*
+        exceptionController.iniciarControl(app);
+
+        healthController.configurarRutas(app);
+        productoController.configurarRutas(app);
+        */
+
+        app.before(ctx -> ctx.header("Content-Type", "application/json"));
+
+        app.start(7000);
     }
 }
