5a8046d866c01743d88aaa541e7d1beaaef9ef29
jenkner305 <johanarteaga215@gmail.com>
2025-06-01 19:06:19 -0500

    se agrego controlador health

---
+++ src/main/java/tienda/controlador/HealthController.java
@@ -0,0 +1,13 @@
+package tienda.controlador;
+
+import io.javalin.Javalin;
+
+public class HealthController {
+
+    public void configurarRutas(Javalin app) {
+        app.get("/health", ctx -> {
+            ctx.result("OK");
+            ctx.status(200);
+        });
+    }
+}
